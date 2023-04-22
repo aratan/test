ae19262a2347684294da0acf2c434d0b7bffa87e
aratancoders <130927652+aratancoders@users.noreply.github.com>
2023-04-22 12:13:03 +0200

    pruebas unitarias

---
+++ test_app.py
@@ -0,0 +1,15 @@
+import pytest
+from flask import Flask, url_for
+
+@pytest.fixture
+def client():
+    app = Flask(__name__)
+    app.config['TESTING'] = True
+
+    with app.test_client() as client:
+        yield client
+
+def test_hello(client):
+    response = client.get('/')
+    assert response.status_code == 404
+    
